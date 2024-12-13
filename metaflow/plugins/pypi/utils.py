8ba52cd8954fb1aff91a2ad1d2711e4b81f87b01
Sakari Ikonen <sakari.a.ikonen@gmail.com>
2024-12-13 18:53:27 +0200

    add mirror url for downloading micromamba. add fallback logic for download

--- metaflow/plugins/pypi/utils.py
+++ metaflow/plugins/pypi/utils.py
@@ -21,6 +21,9 @@ from urllib.parse import unquote, urlparse
 
 from metaflow.exception import MetaflowException
 
+MICROMAMBA_URL = "https://micro.mamba.pm/api/micromamba/{platform}/2.0.4"
+MICROMAMBA_MIRROR_URL = "https://mirror-to/micromamba/{platform}/2.0.4"
+
 
 def conda_platform():
     # Returns the conda platform for the Python interpreter
